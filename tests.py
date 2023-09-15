10cb800af4f30d6480ee3e48c853ad9bc50f4b6c
unknown <ndiuel@gmail.com>
2023-09-15 23:43:03 +0100

    updated tests

--- tests.py
+++ tests.py
@@ -1,33 +1,36 @@
 import requests
 
 def test_create():
-    r = requests.post('http://localhost:3000/api', json={'name': 'Samuel', 'age': 25, 'email': 'ndiuel@gmail.com'})
+    r = requests.post('https://hng-stage-2-ndiuel.vercel.app/api', json={'name': 'Samuel', 'age': 25, 'email': 'ndiuel@gmail.com'})
 
     data = r.json()
     assert data['name'] == 'Samuel'
     assert data['age'] == 25
     assert data['email'] == 'ndiuel@gmail.com'
     assert 'id' in data
+    print("--------------Create Record Passed--------------")
 
 
 def test_fetch_all():
-    r = requests.get('http://localhost:3000/api')
+    r = requests.get('https://hng-stage-2-ndiuel.vercel.app/api')
 
     data = r.json()[0]
     assert 'name' in data
     assert 'age' in data
     assert 'email' in data
     assert 'id' in data
+    print("--------------Fetch Records Passed--------------")
 
 
 def test_fetch():
-    r = requests.get('http://localhost:3000/api/Samuel')
+    r = requests.get('https://hng-stage-2-ndiuel.vercel.app/api/Samuel')
     data = r.json()
     assert data['name'] == 'Samuel'
 
-    r = requests.get(f'http://localhost:3000/api/{data["id"]}')
+    r = requests.get(f'https://hng-stage-2-ndiuel.vercel.app/api/{data["id"]}')
     data = r.json()
     assert data['name'] == 'Samuel'
+    print("--------------Fetch Single Record Passed--------------")
 
 
 def test_update():
@@ -37,17 +40,19 @@ def test_update():
     }
 
 
-    r = requests.patch('http://localhost:3000/api/Samuel', json=body)
+    r = requests.patch('https://hng-stage-2-ndiuel.vercel.app/api/Samuel', json=body)
     data = r.json()
     assert data['age'] == 15
     assert data['email'] == 'ndi@gmail.com'
+    print("--------------Update Record Passed--------------")
 
 
 def test_delete():
 
-    r = requests.delete('http://localhost:3000/api/Samuel')
+    r = requests.delete('https://hng-stage-2-ndiuel.vercel.app/api/Samuel')
     data = r.json()
     assert data['message'].lower() == 'samuel has been deleted'
+    print("--------------Delete Record Passed--------------")
 
 
 if __name__ == '__main__':
